ad8864230f9e4c9193859a9129167c5c6cb76e4b
SarahIaquinta <sarah.r.iaquinta@gmail.com>
2022-10-21 18:44:29 +0200

    documented function

--- uptake/__init__.py
+++ uptake/__init__.py
@@ -2,8 +2,20 @@ from path import Path
 
 root_path = Path(__file__).abspath().parent
 
-
 def make_data_folder(folder_name):
+    """Find the path to a folder
+
+    Parameters:
+    ----------
+    folder_name: string
+        name of the folder
+
+    Returns:
+    -------
+    path_to_data_folder: string (Path)
+        path to the folder
+
+    """
     path_to_data_folder = root_path / folder_name
     Path.mkdir_p(path_to_data_folder)
     return path_to_data_folder
